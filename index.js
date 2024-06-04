60988eb3225d8e4abca6962fd3ea2ed5e90b24e3
Aliyv019 <tahiraliyev2006@gmail.com>
2024-06-04 08:58:04 +0400

    js-task6 added

---
+++ index.js
@@ -0,0 +1,40 @@
+//JS-TASK6
+var text=['apple','banana','kiwi','cherry']
+//task1
+var i=0
+while (i<=10) {
+    console.log(i);
+    i++
+}
+console.log("");
+//task2
+i=0
+while (i<text.length) {
+    console.log(text[i]);
+    console.log(i);
+    i++
+}
+console.log("");
+
+//task3
+i=0
+while (i<text.length) {
+    console.log(i);
+    i++
+}
+console.log("");
+
+//task4
+i=0
+while(i<text.length){
+    console.log(text[i]);
+    i++
+}
+console.log("");
+
+//task5
+i=0
+while (i<text.length) {
+    console.log(text[i]);
+    i++
+}
